3f7baa5fd01ca325bec1a8e77a47fe5dfd53e29d
William Martens <68499986+loneicewolf@users.noreply.github.com>
2021-01-29 11:23:03 +0100

    Update fhide.c

--- fhide/fhide.c
+++ fhide/fhide.c
@@ -5,7 +5,7 @@
 /*
 Example:
 //prefix = the prefix to hide.
-//kpath =  the dir to "operte" on / inside of.  Inside this directory the files with the chosen prefix will be hidden.
+//kpath =  the dir to "operate" on / inside of.  Inside this directory the files with the chosen prefix will be hidden.
 
 sudo insmod fhide.ko prefix="_SomeVeryLongPrefix" path="/home/user/"
 
